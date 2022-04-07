0e668c5baac296a81a2c665bfe9c8bbffb1b4105
zimberzimber <levshakhgeldian@gmail.com>
2022-04-07 16:13:01 +0300

    iseq-b-gon

--- OS specific actions/common.rb
+++ OS specific actions/common.rb
@@ -15,7 +15,8 @@ def rake(root)
     raise "Missing `Rakefile` at: #{ruby_files}" if !File.exist?(ruby_files + "/Rakefile")
 
     Dir.chdir(ruby_files)
-    system("rake build_iseq")
+    # system("rake build_iseq")
+    system("rake")
     Dir.chdir(original_dir)
     FileUtils.rm_rf(ruby_files)
 end
